30d10b319921c49ecc4bffd162ec12c893301b9f
rumina.merchant <rumina.merchant@LQA-RUMINA>
2021-03-30 21:23:10 +0500

    CorcentricTest

---
+++ Corcentric/PageObjects/PracticeForm.cs
@@ -0,0 +1,100 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using SeleniumExtras.PageObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Corcentric.PageObjects
+{
+    class PracticeForm
+    {
+        IWebDriver driver;
+        public PracticeForm(IWebDriver driver)
+        {
+            this.driver = driver;
+            PageFactory.InitElements(driver, this);
+        }
+        public void GotoUrl()
+        {
+            driver.Navigate().GoToUrl("https://demoqa.com/");
+            IJavaScriptExecutor js = (IJavaScriptExecutor)driver;
+            js.ExecuteScript("window.scrollBy(0,document.body.scrollHeight)");
+        }
+
+        [FindsBy(How = How.XPath, Using = "//h5[contains(text(),'Forms')]")]
+        private IWebElement Form;
+        [FindsBy(How = How.XPath, Using = "//span[contains(text(),'Practice Form')]")]
+        private IWebElement Practice;
+        [FindsBy(How = How.CssSelector, Using = "#firstName")]
+        private IWebElement FirstName;
+        [FindsBy(How = How.CssSelector, Using = "#lastName")]
+        private IWebElement LastName;
+        [FindsBy(How = How.XPath, Using = "//label[contains(text(),'Female')]")]
+        private IWebElement Gender;
+        [FindsBy(How = How.CssSelector, Using = "#userNumber")]
+        private IWebElement Number;
+        [FindsBy(How = How.CssSelector, Using = "#submit")]
+        private IWebElement Submit;
+        [FindsBy(How = How.CssSelector, Using = "#example-modal-sizes-title-lg")]
+        private IWebElement Text;
+
+
+        public void openForm()
+        {
+            Form.Click();
+        }
+        public void openpractiseform()
+        {
+            Practice.Click();
+        }
+        public void SetGender()
+        {
+            Gender.Click();
+        }
+        public void SetFirstName(string name)
+        {
+            FirstName.SendKeys(name);
+        }
+        public void SetLastName(string name)
+        {
+            LastName.SendKeys(name);
+        }
+        public void SetNumber(string number)
+        {
+            Number.SendKeys(number);
+        }
+        public void Save()
+        {
+            IJavaScriptExecutor js = (IJavaScriptExecutor)driver;
+            js.ExecuteScript("arguments[0].scrollIntoView();", Submit);
+
+            Submit.Click();
+        }
+
+        public void Assertion(string expected)
+        {
+            String actual = Text.Text.Trim();
+            Assert.AreEqual(expected, actual);
+        }
+
+        public void FiledValidation()
+        {
+         var result=   driver.FindElements(By.CssSelector(".form-control:invalid"));
+
+            foreach (var v in result)
+            {
+                if (v.GetAttribute("id") != "firstName" && v.GetAttribute("id") != "lastName" && v.GetAttribute("id") != "userNumber")
+                {
+                    Assert.Fail(v.GetAttribute("id") + "not found");
+                }
+             
+            }            
+            
+        }
+ 
+
+    }
+}
---
+++ Corcentric/TestClass.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using NUnit;
+using NUnit.Framework;
+using Corcentric.PageObjects;
+using SeleniumExtras.PageObjects;
+
+namespace Corcentric
+{
+    [TestClass]
+    public class TestClass
+    {
+     public static IWebDriver driver;
+        [TestInitialize]
+        public void Setup()
+        {
+            string path = System.IO.Path.GetFullPath(@"..\..\WinDriver\");
+            driver = new ChromeDriver(path);
+            driver.Manage().Window.Maximize();
+           
+        }
+        [TestMethod]
+        public void Case1()
+        {
+            var PracticeForm = new PracticeForm(driver);
+            PracticeForm.GotoUrl();
+            PracticeForm.openForm();
+            PracticeForm.openpractiseform();
+            PracticeForm.SetFirstName("Rumina");
+            PracticeForm.SetLastName("Merchant");
+            PracticeForm.SetGender();
+            PracticeForm.SetNumber("012344566787542");
+            PracticeForm.Save();
+            PracticeForm.Assertion("Thanks for submitting the form");
+        }
+
+        [TestMethod]
+        public void Case2()
+        {
+            var PracticeForm = new PracticeForm(driver);
+            PracticeForm.GotoUrl();
+            PracticeForm.openForm();
+            PracticeForm.openpractiseform();
+            PracticeForm.Save();
+            PracticeForm.FiledValidation();
+        }
+        [TestCleanup]
+        public void QuitDriver()
+        {
+            driver.Quit();
+        }
+       
+    }
+}
